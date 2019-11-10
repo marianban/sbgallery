005b272dbce7535137dbfd7fd8f784cb3c5242ad
Marian Ban <marian.ban@gmail.com>
2019-11-10 16:10:00 +0100

    new products

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `products2`,
+        name: `products6`,
         path: `${__dirname}/src/images/products`,
       },
     },
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export const query = graphql`
     images: allFile(
       filter: {
         extension: { regex: "/(jpeg|jpg|gif|png)/" }
-        sourceInstanceName: { eq: "products2" }
+        sourceInstanceName: { eq: "products6" }
       }
     ) {
       edges {
@@ -30,27 +30,30 @@ export const query = graphql`
   }
 `
 
-const IndexPage = ({ data: { images } }) =>
- (
-    <Layout>
-      <SEO title="For small businesses, individuals and booth rentals" lang="en" description="Showcase of the best Salon and Barber software available for booth renters. Choose from more than hundred online service providers." />
-      <Helmet>
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-          crossorigin="anonymous"
-        />
-        <script
-          src="https://kit.fontawesome.com/6d995c7810.js"
-          crossorigin="anonymous"
-        ></script>
-      </Helmet>
-      <Hero />
-      <SearchArea />
-      <Products images={images} />
-      <Newsletter />
-    </Layout>
-  )
+const IndexPage = ({ data: { images } }) => (
+  <Layout>
+    <SEO
+      title="For small businesses, individuals and booth rentals"
+      lang="en"
+      description="Showcase of the best Salon and Barber software available for booth renters. Choose from more than hundred online service providers."
+    />
+    <Helmet>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+        crossorigin="anonymous"
+      />
+      <script
+        src="https://kit.fontawesome.com/6d995c7810.js"
+        crossorigin="anonymous"
+      ></script>
+    </Helmet>
+    <Hero />
+    <SearchArea />
+    <Products images={images} />
+    <Newsletter />
+  </Layout>
+)
 
 export default IndexPage
